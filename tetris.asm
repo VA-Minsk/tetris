a57d53508852ecc49cdb072cdee26d8ce2fc8005
AJIOB <aleksandr9809@gmail.com>
2017-03-26 17:35:46 +0300

    Init commit. Field is made

---
+++ tetris.asm
@@ -0,0 +1,199 @@
+;???? "??????"
+
+
+;macro HELP
+endd MACRO
+	mov ah, 4ch
+	int 21h
+ENDM
+
+clearScreen MACRO
+	push ax
+	mov ax, 0003h
+	int 10h
+	pop ax
+ENDM
+;end macro help
+
+.model small
+
+.stack 100h
+
+.data
+
+Timer 		dw 	0
+LevelWait   dw	LEVEL_WAIT_INIT
+Level		dw	1
+StartLevel	dw	0
+Pos			dw	START_POS
+Item		dw	0
+Rotated		dw	0
+Drops		dw  0
+RandSeed    dw	0
+PreView		dw	0
+Paused      db  0
+HiScoreData db	14,0
+HiScoreName db	15 dup ('$')
+HiScore 	dd	0
+OldInt1c 	db  4 dup (?)
+Score		dw	2 dup (?)
+OldRand		dw	1 dup (?)
+PreItem		dw	1 dup (?)
+TimerInit	dw	1 dup (?)
+
+;key bindings (configuration)
+KRotate equ 48h	    ;Up key
+KLeft equ 4Bh	    ;Left key
+KRight equ 4Dh		;Right key
+KExit equ 01h 		;ESC key
+
+xSize equ 80
+ySize equ 18
+xField equ 25           ;size of playground
+yField equ 14
+oneMemoBlock equ 2
+scoreSize equ 4
+
+videoStart dw 0B800h
+dataStart dw 0000h
+timeStart dw 0040h
+timePosition dw 006Ch
+
+space equ 0020h
+Figure1Symbol equ 0AEFh
+Figure2Symbol equ 0B0Fh
+VWallSymbol equ 0FBAh
+HWallSymbol equ 0FCDh
+VWallSpecialSymbol equ 0FCCh
+
+fieldSpacingBad equ space, VWallSymbol, xField dup(space)
+fieldSpacing equ fieldSpacingBad, VWallSymbol
+rbSym equ 0CFDCh	;white block with red background
+rbSpc equ 0CF20h	;space with red background
+grSym equ 02FDBh	;white block with green background
+grSpc equ 02F20h	;space with green background
+
+screen	dw xSize dup(space)
+		dw space, 0FC9h, xField dup(HWallSymbol), 0FCBh, xSize - xField - 5 dup(HWallSymbol), 0FBBh, space
+firstBl	dw fieldSpacing, xSize - xField - 5 dup(rbSpc), VWallSymbol, space
+		dw fieldSpacing, 2 dup(rbSpc), 7 dup(rbSym), 3 dup(rbSpc),4 dup(rbSym), 3 dup(rbSpc), 7 dup(rbSym), 3 dup(rbSpc),3 dup(rbSym), 3 dup(rbSpc),5 dup(rbSym), 3 dup(rbSpc), 5 dup(rbSym) ,2 dup(rbSpc), VWallSymbol, space
+		dw fieldSpacing, 5 dup(rbSpc), rbSym, 6 dup(rbSpc),rbSym, 9 dup(rbSpc), rbSym, 6 dup(rbSpc),rbSym, 2 dup(rbSpc), rbSym, 4 dup(rbSpc), rbSym, 5 dup(rbSpc), rbSym, 6 dup(rbSpc) , VWallSymbol, space
+		dw fieldSpacing, 5 dup(rbSpc), rbSym, 6 dup(rbSpc),4 dup (rbSym), 6 dup(rbSpc), rbSym, 6 dup(rbSpc),3 dup (rbSym), 5 dup(rbSpc), rbSym, 5 dup(rbSpc), 5 dup (rbSym) , 2 dup(rbSpc) , VWallSymbol, space
+		dw fieldSpacing, 5 dup(rbSpc), rbSym, 6 dup(rbSpc),rbSym, 9 dup(rbSpc), rbSym, 6 dup(rbSpc) ,rbSym, rbSpc, rbSym,5 dup(rbSpc), rbSym, 9 dup(rbSpc), rbSym, 2 dup(rbSpc) , VWallSymbol, space
+		dw fieldSpacing, 5 dup(rbSpc), rbSym, 6 dup(rbSpc),4 dup (rbSym), 6 dup(rbSpc), rbSym, 6 dup(rbSpc),rbSym, 2 dup(rbSpc), rbSym, 2 dup(rbSpc), 5 dup (rbSym), 3 dup(rbSpc), 5 dup(rbSym), 2 dup(rbSpc) , VWallSymbol, space
+		dw fieldSpacing, xSize - xField - 5 dup(rbSpc), VWallSymbol, space  
+		
+delim1	dw fieldSpacingBad, 0FCCh, xSize - xField - 5 dup(HWallSymbol), 0FB9h, space  
+
+secondF	dw fieldSpacing, xSize - xField - 5 dup(grSpc), VWallSymbol, space
+		dw fieldSpacing, 12 dup (grSpc), 7 dup (grSym), 5 dup (grSpc),grSym, 3 dup (grSpc), grSym, 5 dup (grSpc), 4 dup (grSym), 12 dup (grSpc), VWallSymbol, space
+        dw fieldSpacing, 15 dup (grSpc), grSym, 8 dup (grSpc),grSym, 3 dup (grSpc), grSym, 5 dup (grSpc), grSym, 15 dup (grSpc), VWallSymbol, space	
+        dw fieldSpacing, 15 dup (grSpc), grSym, 9 dup (grSpc),grSym, 1 dup (grSpc), grSym, 6 dup (grSpc), 4 dup (grSym), 12 dup (grSpc), VWallSymbol, space	
+        dw fieldSpacing, 15 dup (grSpc), grSym, 9 dup (grSpc),grSym, 1 dup (grSpc), grSym, 9 dup (grSpc), grSym, 12 dup (grSpc), VWallSymbol, space	                                                                                                                                                                     
+        dw fieldSpacing, 15 dup (grSpc), grSym, 10 dup (grSpc),grSym, 7 dup (grSpc), 4 dup (grSym), 12 dup (grSpc), VWallSymbol, space	
+	    dw fieldSpacing, xSize - xField - 5 dup(grSpc), VWallSymbol, space                                                                                                                                                            
+		  		
+		dw space, 0FC8h, xField dup(HWallSymbol), 0FCAh, xSize - xField - 5 dup(HWallSymbol), 0FBCh, space
+		dw xSize dup(space)
+.code
+
+main:
+	mov ax, @data	;init
+	mov ds, ax
+	mov dataStart, ax
+	mov ax, videoStart
+	mov es, ax
+	xor ax, ax
+
+	clearScreen
+
+	call initAllScreen
+
+	call mainGame
+
+to_close:
+	;clearScreen
+
+	endd
+
+;more macro help
+
+;ZF = 1 - buffer is free
+;AH = scan-code
+CheckBuffer MACRO
+	mov ah, 01h
+	int 16h
+ENDM
+
+ReadFromBuffer MACRO
+	mov ah, 00h
+	int 16h
+ENDM
+
+;result in cx:dx
+GetTimerValue MACRO
+	push ax 
+
+	mov ax, 00h
+	int 1Ah
+
+	pop ax
+ENDM
+
+;end macro help
+
+;procedure help
+
+initAllScreen PROC
+	mov si, offset screen
+	xor di, di
+
+	mov cx, xSize*ySize
+	rep movsw    
+	
+mainGame PROC
+	call    RandInit  				; Initialize random numbers
+
+
+
+    ret
+ENDP
+	
+RandInit       proc    near
+	mov     bp,sp
+	sub     sp,4
+	mov      ah,02h
+	int      1ah
+	mov      [bp-2],cl
+	mov      [bp-4],dh
+	mov     ax,word ptr [bp-2]
+	imul    word ptr [bp-4]
+	mov     word ptr randseed,ax
+	mov     sp,bp
+	call    Rand7
+	ret
+RandInit       endp
+
+Rand7  proc    near
+	mov     ax, word ptr randseed
+	imul    word ptr randseed
+
+	test    ax,1
+	je      short even_number
+	add     ax,3172
+	mov     word ptr randseed,ax
+	jmp     short skip_even
+even_number:
+	shl		ax,1
+	add		ax,Score
+skip_even:
+	mov     word ptr randseed,ax
+	mov     bx,7
+	xor     dx,dx
+	div     bx
+	mov     ax, word ptr OldRand
+	mov		word ptr OldRand, dx
+	ret
+Rand7  endp
+
+end main
