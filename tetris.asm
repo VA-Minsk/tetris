2cb58d9f10dd17fc4eb1d66f484c64d9ce6f1701
AJIOB <aleksandr9809@gmail.com>
2017-03-30 16:58:13 +0300

    GenerateNewBlock and Random procedures are written. Remove unnecessary bodies of procedures

--- tetris.asm
+++ tetris.asm
@@ -29,8 +29,6 @@ videoStart dw 0B800h
 dataStart dw 0000h
 
 space equ 0020h
-Figure1Symbol equ 0AEFh
-Figure2Symbol equ 0B0Fh
 VWallSymbol equ 0FBAh
 HWallSymbol equ 0FCDh
 VWallSpecialSymbol equ 0FCCh
@@ -65,16 +63,61 @@ secondF	dw fieldSpacing, xSize - xField - 5 dup(grSpc), VWallSymbol, space
 		dw space, 0FC8h, xField dup(HWallSymbol), 0FCAh, xSize - xField - 5 dup(HWallSymbol), 0FBCh, space
 		dw xSize dup(space)
 
-;	Figures
-maxViewNum equ 4
+;	figures
+figureBlock equ grSym
 
-emptyFig equ 0
-fullFig equ 0ffh
+maxViewNum equ 4
+currViewNum db 0
+
+emptyBlock equ 0
+fullBlock equ 0ffh
+
+fieldSize equ xField*yField
+oneFigureBigSize equ maxViewNum*fieldSize
 
 ;	save: figure - row number - column number
-currentFigure db maxViewNum*xField*yField dup(emptyFig)
-fututeFigure db maxViewNum*xField*yField dup(emptyFig)
-virtualField db xField*yField dup(emptyFig)
+currentFigure db oneFigureBigSize dup(emptyBlock)
+futureFigure db oneFigureBigSize dup(emptyBlock)
+virtualField db fieldSize dup(emptyBlock)
+
+numOfFigures equ 7
+
+
+;	квадратик
+figures	db fieldSize dup(emptyBlock)
+		db fieldSize dup(emptyBlock)
+		db fieldSize dup(emptyBlock)
+		db fieldSize dup(emptyBlock)
+	;	I
+		db fieldSize dup(emptyBlock)
+		db fieldSize dup(emptyBlock)
+		db fieldSize dup(emptyBlock)
+		db fieldSize dup(emptyBlock)
+	;	T
+		db fieldSize dup(emptyBlock)
+		db fieldSize dup(emptyBlock)
+		db fieldSize dup(emptyBlock)
+		db fieldSize dup(emptyBlock)
+	;	z
+		db fieldSize dup(emptyBlock)
+		db fieldSize dup(emptyBlock)
+		db fieldSize dup(emptyBlock)
+		db fieldSize dup(emptyBlock)
+	;	z зеркальное
+		db fieldSize dup(emptyBlock)
+		db fieldSize dup(emptyBlock)
+		db fieldSize dup(emptyBlock)
+		db fieldSize dup(emptyBlock)
+	;	Г
+		db fieldSize dup(emptyBlock)
+		db fieldSize dup(emptyBlock)
+		db fieldSize dup(emptyBlock)
+		db fieldSize dup(emptyBlock)
+	;	Г зеркальное
+		db fieldSize dup(emptyBlock)
+		db fieldSize dup(emptyBlock)
+		db fieldSize dup(emptyBlock)
+		db fieldSize dup(emptyBlock)
 
 .code
 
@@ -153,6 +196,9 @@ checkTimeLoop:
 ENDP
 
 mainGame PROC
+	call GenerateNewBlock
+	call PrintCurrFigure
+
 ;	Main loop
 ;	*********
 checkKeyPressing:
@@ -162,7 +208,7 @@ checkKeyPressing:
 	ReadFromBuffer
 
 	cmp		al, KExit
-	je 		quit
+	je 		game_over
 	cmp		al, KLeft
 	je 		go_left
 	cmp		al, KRight
@@ -173,7 +219,7 @@ checkKeyPressing:
 ;	je 		go_drop
 
 ;	If not found key - down block & go to new iteration
-
+newIteration:
 	call    Down                  ; go down
 	cmp		ax, 0
 	jne		game_over
@@ -192,11 +238,6 @@ go_right:
 	mov 	al, 0
 	call    Move
 	jmp     checkKeyPressing
-;go_drop:
-;	call    Drop
-;	or		ax,ax
-;	je		game_over
-;	jmp     checkKeyPressing
 go_rotate:
 	call    Rotate
 	jmp     checkKeyPressing
@@ -207,280 +248,99 @@ game_over:
     ret
 ENDP
 
-Drop	proc
-	push	si
-	mov		si, 2*xSize			;was 80
-drop_more:
-	call	Move
-	inc		word ptr Drops
-	or		ax,ax
-	jne		drop_more
+;	Time random
+;	Input:
+;		ax - max value
+;	Result:
+;		ax - random number [0, max value)
+Random PROC
+	push bx cx dx
 
-	call	Down
-	pop		si
+	mov cx, ax			;в cx - max value
+	cmp cx, 0
+	je quitRandom
+
+	;считываем текущее время
+	;ch - час, cl - минута, dh - секунда, dl - сотая доля секунды
+	mov ah, 2Ch
+	int 21h
+
+	mov al, dl
+	mul dh 				;в ax теперь число для рандома
+
+	xor dx, dx			;чтобы не словить переполнение и получить хороший результат
+	div cx				;в dx - результат (случайное число);
+
+	mov ax, dx
+
+quitRandom:
+	pop dx cx bx
 	ret
-endp
+ENDP
+
+GenerateNewBlock PROC
+	push ax cx ds es si di
+
+	mov ax, numOfFigures
+	call Random
+	mov cx, oneFigureBigSize	;в cx - размер одной "пачки" из видов фигур
+	mul cx						;теперь имеем смещение фигуры относительно начала figures
+
+	mov si, offset figures
+	add si, ax					;получаем реальное смещение в памяти, где сгенерированная фигура
+	mov ax, dataStart
+	mov ds, ax
+	mov es, ax
+	mov di, offset currentFigure	;теперь всё готово для "копирования" новой фигуры
+	rep movsb
+
+	;Выбираем вид фигуры
+	mov ax, maxViewNum
+	call Random
+	mov currViewNum, al				;сохраняем индекс текущего вида
+
+	pop di si es ds cx ax
+	ret
+ENDP
+
+PrintCurrFigure PROC
+	;todo
+
+	ret
+ENDP
+
+DeleteCurrFigure PROC
+	;todo
+
+	ret
+ENDP
 
 ;	Down block for one position
 ;	If need - generate new one
 ;	Return:
 ;		ax = 0 - all is good
 ;		ax = 1 - no more plase in field
-Down	proc
-	push 	si
-	mov		si, 2*xSize			;was 80
-	call	Move
-
-	or		ax,ax						; room?
-	je		get_new							; no
-	pop		si
-	ret
-get_new:
-	mov		ax,word ptr Item
-	add		ax,word ptr Rotated
-	mov		di,word ptr Pos
-	call	Bottom			; reached the bottom
-
-	call	Rand7			; get new item
-	shl		ax,1
-	shl		ax,1
-	mov		word ptr Item,ax
-	mov		word ptr Pos,START_POS
-	mov		word ptr Rotated,0
-
-;	Test if new tetris can be printed
-	mov		ax,word ptr Item
-	add		ax,word ptr Rotated
-	mov		di,Pos
-	call	TestSpace
-	or		ax,ax
-	jne		place_new
-
-;	There was no room. Return false!
-	pop		si
-	ret
-
-; 	There was room. Print tetris.
-place_new:
-	mov		si,1
-	mov		ax,word ptr Item
-	add		ax,word ptr Rotated
-	mov		di,Pos
-	call	Print
-	mov		ax,	word ptr TimerInit
-	mov		word ptr Timer,ax
-
-no_preview:
-	mov     ax,1
-	pop 	si
-	ret
-endp
-
-;ax - item number ???
-Print	proc					;ax= item_nr    di=scroffs si=T/F
-	mov		dx,0b800h
-	mov		es,dx
-	mov 	dx,06						; 06 - количество элементов в строке Items
-	imul    dx                      	; ax=ax*6
-	add 	ax, offset Items      		; ax+=[_items]
-
-	xor		dx,dx		   				; for dx=3 to 0
-
-	cmp		si,0
-	je 		loop2
-	mov 	bx,ax
-	mov		si,ds:[bx]
-loop2:
-	mov 	bx,ax
-	add 	bx,dx
-	push	ax
-	mov 	al,byte ptr [bx+2]
-	cbw								; байт al в слово в ax (размножением знакового бита)
-	shl		ax,1
-	add		di,ax
-	pop		ax
-	mov 	es:[di],si
-	inc     dx
-	cmp		dx,4
-	jne	    loop2
-	ret
-endp
-
-Bottom	proc      	;ax=ItemNr			di=scroffset
-	mov		dx,0b800h
-	mov		es,dx
-	mov 	dx,06
-
-	imul    dx
-	add 	ax, offset Items     			; ax=ax*6
-
-	xor 	cx,cx                           ; for cx=0 to 3
-loop_b1:
-	mov 	bx,ax
-	add 	bx,cx
-	push	ax
-	mov 	al,byte ptr [bx+2]
-	cbw
-	shl		ax,1
-	add 	di,ax
-	mov		ax,di
-	xor		dx,dx
-	mov		bx,80
-	div		bx
-	mov		bx,ax
-	inc		byte ptr RowFill[bx]
-	pop		ax
-	inc     cx
-	cmp		cx,4
-	jne	    loop_b1
-
-	xor		bx,bx
-loop_b2:
-	cmp		byte ptr RowFill[bx],10
-	jne		not_full
-	push	bx
-	call	DeleteRow
-	inc		word ptr randseed
-	pop		bx
-not_full:
-	inc 	bx
-	cmp		bx,24
-	jne		loop_b2
+Down proc
+	;todo
 
 	ret
 endp
 
-TestSpace	proc		;ax= item_nr    di=scroffs
-	mov		dx,0b800h
-	mov		es,dx
-	mov 	dx,06
-	imul    dx
-	add 	ax, offset Items     			; ax=ax*6
+DeleteRow proc
+	;todo
 
-	xor 	dx,dx                           ; for dx=0 to 3
-loop1:
-	mov 	bx,ax
-	add 	bx,dx
-	push	ax
-	mov 	al,byte ptr [bx+2]
-	cbw
-	shl		ax,1
-	add 	di,ax
-	pop		ax
-	cmp 	word ptr es:[di],0
-	jne	 	bad
-	inc     dx
-	cmp		dx,4
-	jne	    loop1
-	mov		ax,01
-	ret
-bad:
-	xor		ax,ax
 	ret
 endp
 
-DeleteRow	proc				;bx=Row
+Move proc
+	;todo
 
-	mov		dx,0b800h
-	mov		es,dx
-
-	dec		bx
-	mov		cx,bx
-	mov		ax,bx
-
-	mov		dx,80
-	imul	dx
-	add		ax,30						   		; ax=source offset
-
-	mov		si,ax
-loop_d1:
-	mov		di,si
-	add		di,80
-	push 	cx
-	mov     cx,10
-	push	ds
-	push	es
-	pop		ds
-	rep	movsw
-	pop 	ds
-	sub		si,100
-
-	mov 	cl,byte ptr RowFill[bx]
-	mov		byte ptr RowFill[bx+1],cl
-	dec		bx
-
-	pop		cx
-	loop   	loop_d1
-
-	ret
-DeleteRow	endp
-
-Move proc					; si=dPos
-	push 	si
-
-	xor		si,si
-	mov		ax,word ptr Item
-	add		ax,word ptr Rotated
-	mov		di,word ptr Pos
-	call	Print				; remove old
-
-	mov		ax,word ptr Item
-	add		ax,word ptr Rotated
-	mov 	di,word ptr Pos
-	pop 	si
-	add		di,si
-	push 	si
-	call	TestSpace				; test if room
-	pop     si
-	push	ax
-	or		ax,ax
-	je		no_room
-	add		word ptr Pos,si				; ok, add Pos
-no_room:
-	push	si
-	mov 	si,1
-	mov		ax,word ptr Item
-	add		ax,word ptr Rotated
-	mov		di,word ptr Pos
-	call	Print
-	pop		si
-	pop		ax
 	ret
 endp
 
 Rotate	proc
-	push si
-	xor 	si,si
-	mov		ax,word ptr Item
-	add		ax,word ptr Rotated
-	mov		di,word ptr Pos
-	call	Print
+	;todo
 
-	mov		ax,word ptr Rotated
-	inc		ax
-	mov		bx,4
-	cwd
-	idiv	bx
-	mov		ax,word ptr Item
-	add		ax,dx
-	mov		di,word ptr Pos
-	call	TestSpace
-	or		ax,ax
-	je		no_room1
-	mov		ax,word ptr Rotated
-	inc		ax
-	mov		bx,4
-	cwd
-	idiv	bx
-	mov		word ptr Rotated,dx
-no_room1:
-
-	mov		si,1
-	mov		ax,word ptr Item
-	add		ax,word ptr Rotated
-	mov		di,Pos
-	call	Print
-	pop		si
 	ret
 endp
 
